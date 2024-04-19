e742947b452d23860a428bea1eec454d7d14aa10
Mohamed Amr <mohamedamr0001@.com>
2024-04-19 17:16:01 +0200

    s

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# install flask from pip3
+# install a package
 
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip'
+exec { 'flask':
+  command => '/usr/bin/pip3 install flask==2.1.0'
 }
