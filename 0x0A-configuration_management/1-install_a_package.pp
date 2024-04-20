fff962c376b21302727aded289fcca2a274e5dc5
Mohamed Amr <mohamedamr0001@.com>
2024-04-20 05:13:27 +0200

    SG

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
-# Install flask
+# This manifest will install Flask and Werkzeug using pip3
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  name     => 'Werkzeug',
+  provider => 'pip3',
+}
 
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  name     => 'flask',
+  provider => 'pip3',
 }
