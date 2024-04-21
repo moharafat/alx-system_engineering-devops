4435c6a1e4da0bee8de07f7efc2cb6a152a686df
Mohamed Amr <mohamedamr0001@.com>
2024-04-21 12:55:18 +0200

    d

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,24 @@
 #!/usr/bin/env bash
-# Setting up SSH config file
-include stdlib
+file { '/home/ubuntu/.ssh/config':
+  ensure  => present,
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode    => '0600',
+  content => "
+    Host 54.173.50.211
+      IdentityFile ~/.ssh/school
+      PasswordAuthentication no
+  ",
+}
 
-file_line { 'SSH configuration':
+file_line { 'Turn off passwd auth':
   ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  lines   => [
-    '    PasswordAuthentication no',
-    '    IdentityFile ~/.ssh/school',
-  ],
-  replace => true,
+  path   => '/home/ubuntu/.ssh/config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => present,
+  path   => '/home/ubuntu/.ssh/config',
+  line   => '    IdentityFile ~/.ssh/school',
 }
