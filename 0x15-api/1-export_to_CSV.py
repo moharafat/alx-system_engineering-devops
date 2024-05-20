b034b04d33a557f081a50cf39ad92a5b4a77e45e
Mohamed Arafat <mohamedamr0001@gmail.com>
2024-05-21 02:51:53 +0300

    csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
 """script to export data in the CSV format."""
+import csv
 import requests
 import sys
 
 
 if __name__ == "__main__":
     URL = "https://jsonplaceholder.typicode.com/"
-
-
     EMPLOYEE_DATA = requests.get(URL + "users/{}".format(sys.argv[1])).json()
     EMPLOYEE_ID = EMPLOYEE_DATA.get("id")
     EMPLOYEE_USERNAME = EMPLOYEE_DATA.get("username")
 
-    Tasks_4_emply = requests.get(
-        URL + "todos/", params={"userId": sys.argv[1]}).json()
-    COMPLETED_TASKS = []
-    for task in Tasks_4_emply:
-        if task.get("completed"):
-            COMPLETED_TASKS.append(task.get("title"))
-    len_T_C = (len(COMPLETED_TASKS))
-    len_e_t_c = (len(Tasks_4_emply))
-    print("Employee {} is done with tasks({}/{}):".format(
-        EMPLOYEE_NAME, len_T_C, len_e_t_c))
-    for task in COMPLETED_TASKS:
-        print("\t {}".format(task))
+    todos_response = requests.get(URL + "todos/", params={"userId": sys.argv[1]})
+    todos_response.raise_for_status()
+    todos_data = todos_response.json()
+    file_name = f"{EMPLOYEE_ID}.csv"
+
+    with open(file_name, mode='w', newline='') as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        for task in todos_data:
+            writer.writerow([EMPLOYEE_ID, EMPLOYEE_USERNAME, task.get("completed"), task.get("title")])
+        
