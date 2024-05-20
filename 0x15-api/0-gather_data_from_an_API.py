9e5080691612597fe18eaba1a03c02e091b33f63
Mohamed Arafat <mohamedamr0001@gmail.com>
2024-05-21 00:37:33 +0300

    True gone

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,14 +9,16 @@ if __name__ == "__main__":
     EMPLOYEE_DATA = requests.get(URL + "users/{}".format(sys.argv[1])).json()
     EMPLOYEE_NAME = EMPLOYEE_DATA.get("name")
 
-    EMPLOYEE_TASKS_FOR_SPECIFIC_EMPLOYEE =requests.get(URL + "todos/", params={"userId": sys.argv[1]}).json()
+    Tasks_4_emply = requests.get(
+        URL + "todos/", params={"userId": sys.argv[1]}).json()
     COMPLETED_TASKS = []
     counter = 0
-    for task in EMPLOYEE_TASKS_FOR_SPECIFIC_EMPLOYEE:
-        if task.get("completed") == True:
+    for task in Tasks_4_emply:
+        if task.get("completed"):
             COMPLETED_TASKS.append(task.get("title"))
     len_T_C = (len(COMPLETED_TASKS))
-    len_e_t_c = (len(EMPLOYEE_TASKS_FOR_SPECIFIC_EMPLOYEE))
-    print("Employee {} is done with tasks({}/{}):".format(EMPLOYEE_NAME,len_T_C, len_e_t_c))
+    len_e_t_c = (len(Tasks_4_emply))
+    print("Employee {} is done with tasks({}/{}):".format(
+        EMPLOYEE_NAME, len_T_C, len_e_t_c))
     for task in COMPLETED_TASKS:
         print("\t {}".format(task))
