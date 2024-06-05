219c6d209a0ccb80619a0ea62a17e886d5dc3725
Mohamed Arafat <mohamedamr0001@gmail.com>
2024-06-05 17:19:09 +0300

    ruby

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,13 +1,16 @@
+# Install Ruby
 package { 'ruby':
   ensure => installed,
 }
 
+# Install puppet-lint gem
 exec { 'install_puppet_lint':
   command => 'gem install puppet-lint -v 2.1.1',
   path    => ['/usr/local/bin', '/usr/bin', '/bin'],
   require => Package['ruby'],
 }
 
+# fix error
 exec { 'fix-typo_php':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
